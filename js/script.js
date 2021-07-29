c18f73ad00df307145358357314a2ac1ba0857f1
touros91 <andreageraci91@gmail.com>
2021-07-29 15:33:59 +0200

    Refactoring JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 // chiedo all'utente di inserire i chilometri che vuole percorrere 
 var chilometri = parseInt(prompt('Inserire il numero di chilometri da percorrere'));
+
+document.getElementById('chilometri').innerHTML += chilometri;
+
 // chiedo all'utente la sua età 
 var eta = parseInt(prompt('Inserire l’età'));
+
+document.getElementById('eta').innerHTML += eta;
+
 // moltiplico il prezzo base del biglietto di 0.21€ per i chilometri da percorrere 
 var prezzo = (0.21 * chilometri).toFixed(2);
 
@@ -9,10 +15,11 @@ var prezzo = (0.21 * chilometri).toFixed(2);
 
 if (eta < 18) {
 prezzo = (prezzo - (prezzo * 20 / 100)).toFixed(2);
+document.getElementById('prezzo').innerHTML += prezzo;
 alert('Il prezzo del biglietto é: ' + prezzo);
 } else if (eta > 65) {
 prezzo = (prezzo - (prezzo * 40 / 100)).toFixed(2);
-alert('Il prezzo del biglietto é: ' + prezzo);
+document.getElementById('prezzo').innerHTML += prezzo;
 } else {
-alert('Il prezzo del biglietto é: ' + prezzo);
+    document.getElementById('prezzo').innerHTML += prezzo;
 }
