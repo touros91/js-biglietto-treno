1612b2d00edc1aafc20f3dfbc898da3d131886a7
touros91 <andreageraci91@gmail.com>
2021-07-30 10:58:14 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -11,14 +11,17 @@ document.getElementById('eta').innerHTML += eta;
 // moltiplico il prezzo base del biglietto di 0.21€ per i chilometri da percorrere 
 var prezzo = (0.21 * chilometri).toFixed(2);
 
-// verifico le condizioni se l'utente è minorenne o over 65 per applicare lo sconto rispettivo del 20% o 40% altrimenti si applica il prezzo base 
-
-if (eta < 18) {
-prezzo = (prezzo - (prezzo * 20 / 100)).toFixed(2);
-document.getElementById('prezzo').innerHTML += prezzo;
-} else if (eta > 65) {
-prezzo = (prezzo - (prezzo * 40 / 100)).toFixed(2);
-document.getElementById('prezzo').innerHTML += prezzo;
-} else {
+// verifico le condizioni se l'utente ha inserito dei numeri, se si allora verifico se è minorenne o over 65 per applicare lo sconto rispettivo del 20% o 40% altrimenti si applica il prezzo base 
+if (!isNaN(chilometri) && !isNaN(eta)) {
+    if (eta < 18) {
+    prezzo = (prezzo - (prezzo * 20 / 100)).toFixed(2);
     document.getElementById('prezzo').innerHTML += prezzo;
+    } else if (eta > 65) {
+    prezzo = (prezzo - (prezzo * 40 / 100)).toFixed(2);
+    document.getElementById('prezzo').innerHTML += prezzo;
+    } else {
+        document.getElementById('prezzo').innerHTML += prezzo;
+    }
+} else {
+    alert("Non hai inserito dei valori numerici. Riprova!")
 }
